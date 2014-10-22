167ae17c649d7fa00012750fbd272cca9db58260
Rhett Shipp <rhettshipp@gmail.com>
2014-10-22 12:45:07 -0700

    Finds total number of unique names in a guest list

---
+++ guest_list_analysis.py
@@ -0,0 +1,10 @@
+
+guest_list = []
+filename = '/Users/rhettshipp1/Desktop/Data-Files/Guest_list.txt'
+with open(filename) as f:
+    f.next()    
+    for line in f:
+        guest_list.append(line)
+unique_guest_list = list(set(guest_list))
+print unique_guest_list
+print len(unique_guest_list)
