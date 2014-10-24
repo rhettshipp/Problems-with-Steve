bf341e5d6a5d07ca739946449da684c349b4f931
Rhett Shipp <rhettshipp@gmail.com>
2014-10-23 20:59:50 -0700

    adjustments to the character count
    Before the character count was counting all whitespace as well. To fix
    this, I iterate on a list containing each word (created above with the
    line.split()) and count those.
    
    I also un-indented the print commands because we can call those after
    the file has been closed.

--- line-word-character-counter.py
+++ line-word-character-counter.py
@@ -7,8 +7,9 @@ with open(filename) as f:
     for line in f:
         words = line.split()        
         line_count += 1
-        character_count += len(line)
         word_count += len(words)
-    print line_count
-    print character_count
-    print word_count
+        for word in words:
+            character_count += len(word)
+print line_count
+print character_count
+print word_count
