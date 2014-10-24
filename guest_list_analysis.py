0f09f1c2769ed37a2c5ce718adb0ed647aa783e2
Rhett Shipp <rhettshipp@gmail.com>
2014-10-23 20:57:42 -0700

    efficiency improvements
    Begin with an empty set rather than an empty list. This removes the
    need to create two lists (one containing all names and another
    containing only unique names). The strip command was added to remove
    the ‘\n’ from the names.

--- guest_list_analysis.py
+++ guest_list_analysis.py
@@ -1,10 +1,14 @@
-guest_list = []
+unique_guest = set()
 filename = '/Users/rhettshipp1/Desktop/Data-Files/Guest_list.txt'
 with open(filename) as f:
-    f.next()    
+    f.next()
     for line in f:
-        guest_list.append(line)
-unique_guest_list = list(set(guest_list))
-print guest_list
-print unique_guest_list
-print len(unique_guest_list)
+        unique_guest.add(line.strip())
+print unique_guest
+print len(unique_guest)
+
+"""
+Steve's comments - 
+Some changes to make - 
+
+"""
