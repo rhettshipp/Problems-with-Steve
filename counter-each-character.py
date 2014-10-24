d30a3321648886ff45e7c1b4f7af724b6f583e57
Rhett Shipp <rhettshipp@gmail.com>
2014-10-23 20:54:55 -0700

    several changes
    Abandoned the approach of creating a list of unique characters and then
    iterating on that list. Removed all code related to that.
    
    Instead I begin with an empty dictionary, then create a list containing
    all the words or strings in my file, I then look at each character in
    each word and add them to the dictionary while implementing a counter
    to count each time an item or character appears.

--- counter-each-character.py
+++ counter-each-character.py
@@ -1,19 +1,20 @@
-"""
-Task - count the frequency of each character appearing in the document
-1-create a list of all unique characters
-2-create a function that will take each character in the text file
-and count how many times it appears.
-3-create a command that calls out each unique character and
-reports the frequency of that number appearing
-"""
-total_characters = []
+
+frequency = {}
 filename = '/Users/rhettshipp1/Desktop/Data-Files/random-data.txt'
 with open(filename) as f:
     for line in f:
-        for item in line:
-            total_characters.append(item)    
-unique_characters = list(set(total_characters))
-print total_characters
-print unique_characters
-unique_characters.pop("\n")
-print unique_characters
+        words = line.split()
+        for word in words:
+            for item in word:
+                if item not in frequency:
+                    frequency[item] = 0
+                frequency[item] += 1
+print frequency
+
+"""
+alternatively, my if statement could have looked like:
+if item in frequency:
+    frequency[item] += 1
+else:
+    frequency[item] = 1
+"""
