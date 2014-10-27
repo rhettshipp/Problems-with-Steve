9a304d9dfe63dff80eeb67eef459f255ef7ccfec
Rhett Shipp <rhettshipp@gmail.com>
2014-10-27 15:57:28 -0700

    Initial Commit

---
+++ character-count-by-position.py
@@ -0,0 +1,12 @@
+result = []
+with open('filename') as f:
+    for line in f:
+        for i, char in enumerate(line):
+            if i+1 > len(result):
+                result.append({})
+            if char in result[i]:
+                result[i][char] += 1
+            else:
+                result[i][char] = 1
+print result            
+                
