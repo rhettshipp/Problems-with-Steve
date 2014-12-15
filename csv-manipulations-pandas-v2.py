2b7d31c4c51dbc9bbd172d1a19ece44aad14d23a
Rhett Shipp <rhettshipp@gmail.com>
2014-12-15 15:18:12 -0800

    Delete csv-manipulations-pandas-v2.py

--- csv-manipulations-pandas-v2.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-chemdf = pd.read_csv('/Users/rhettshipp1/Desktop/Problems-with-Steve/Data Files/AID_466_data.csv',
-                     na_values = '')
-shrtchemdf = chemdf[['PUBCHEM_CID',
-                     'PUBCHEM_ACTIVITY_OUTCOME', 
-                     '4^Hill Slope^FLOAT^^^^']]
-
